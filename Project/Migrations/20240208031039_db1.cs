0c2ed2abcdb2b12bfeb89240a00018fffdac9b1f
CARLOS EDUARDO LOBO DIAZ <celobo@uce.edu.ec>
2024-02-07 22:26:48 -0500

    agregados la injecion de dependencia y en layout

---
+++ Project/Migrations/20240208031039_db1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class db1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ApellidoMaterno = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ApellidoPaterno = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NiveLealtad = table.Column<int>(type: "int", nullable: false),
+                    PuntosRecompensa = table.Column<int>(type: "int", nullable: false),
+                    contraseña = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleado",
+                columns: table => new
+                {
+                    idEmpleado = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PrimerNombre = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    SegundoNombre = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ApellidoParterno = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ApellidoMaterno = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CodAdmin = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleado", x => x.idEmpleado);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producto",
+                columns: table => new
+                {
+                    IdProducto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreProducto = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(10,0)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Categoria = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto", x => x.IdProducto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proveedor",
+                columns: table => new
+                {
+                    idProveedor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreProvedor = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    NombreEmpresa = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedor", x => x.idProveedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venta",
+                columns: table => new
+                {
+                    idVenta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImporteTotal = table.Column<decimal>(type: "decimal(10,0)", nullable: false),
+                    CantidadTotal = table.Column<int>(type: "int", nullable: false),
+                    idCliente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venta", x => x.idVenta);
+                    table.ForeignKey(
+                        name: "FK_Venta_Cliente_idCliente",
+                        column: x => x.idCliente,
+                        principalTable: "Cliente",
+                        principalColumn: "IdCliente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compra",
+                columns: table => new
+                {
+                    idCompra = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CostoTotal = table.Column<int>(type: "int", nullable: false),
+                    FechaCompra = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TipoComprobante = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ProveedorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compra", x => x.idCompra);
+                    table.ForeignKey(
+                        name: "FK_Compra_Proveedor_ProveedorId",
+                        column: x => x.ProveedorId,
+                        principalTable: "Proveedor",
+                        principalColumn: "idProveedor",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Detalleventa",
+                columns: table => new
+                {
+                    IdDetalleVenta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IVA = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnidad = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descuento = table.Column<int>(type: "int", nullable: false),
+                    idProducto = table.Column<int>(type: "int", nullable: true),
+                    idVenta = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Detalleventa", x => x.IdDetalleVenta);
+                    table.ForeignKey(
+                        name: "FK_Detalleventa_Producto_idProducto",
+                        column: x => x.idProducto,
+                        principalTable: "Producto",
+                        principalColumn: "IdProducto");
+                    table.ForeignKey(
+                        name: "FK_Detalleventa_Venta_idVenta",
+                        column: x => x.idVenta,
+                        principalTable: "Venta",
+                        principalColumn: "idVenta");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleCompra",
+                columns: table => new
+                {
+                    idDetalleCompra = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaFactura = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CostoTolal = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    IVA = table.Column<int>(type: "int", nullable: false),
+                    EmpleadoId = table.Column<int>(type: "int", nullable: false),
+                    CompraId = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    roductoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleCompra", x => x.idDetalleCompra);
+                    table.ForeignKey(
+                        name: "FK_DetalleCompra_Compra_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compra",
+                        principalColumn: "idCompra",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetalleCompra_Empleado_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleado",
+                        principalColumn: "idEmpleado",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetalleCompra_Producto_roductoId",
+                        column: x => x.roductoId,
+                        principalTable: "Producto",
+                        principalColumn: "IdProducto",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compra_ProveedorId",
+                table: "Compra",
+                column: "ProveedorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleCompra_CompraId",
+                table: "DetalleCompra",
+                column: "CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleCompra_EmpleadoId",
+                table: "DetalleCompra",
+                column: "EmpleadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleCompra_roductoId",
+                table: "DetalleCompra",
+                column: "roductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Detalleventa_idProducto",
+                table: "Detalleventa",
+                column: "idProducto");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Detalleventa_idVenta",
+                table: "Detalleventa",
+                column: "idVenta");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venta_idCliente",
+                table: "Venta",
+                column: "idCliente");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleCompra");
+
+            migrationBuilder.DropTable(
+                name: "Detalleventa");
+
+            migrationBuilder.DropTable(
+                name: "Compra");
+
+            migrationBuilder.DropTable(
+                name: "Empleado");
+
+            migrationBuilder.DropTable(
+                name: "Producto");
+
+            migrationBuilder.DropTable(
+                name: "Venta");
+
+            migrationBuilder.DropTable(
+                name: "Proveedor");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Project.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure the DbContext
+var connectionString = builder.Configuration.GetConnectionString("conexion");
+builder.Services.AddDbContext<SupermercadoContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -10,6 +17,17 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+else
+{
+    // Apply database migrations automatically during development
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetRequiredService<SupermercadoContext>();
+        dbContext.Database.Migrate();
+    }
+}
+
 app.UseStaticFiles();
 
 app.UseRouting();
